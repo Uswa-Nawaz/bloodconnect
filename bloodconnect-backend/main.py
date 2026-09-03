a1c45d53490650a7159a70f72128741ff0df5249
Uswa-Nawaz <uswanawaz08@gmail.com>
2026-09-03 15:53:06 +0500

    Allow port 5501 in CORS origins alongside 5500

--- bloodconnect-backend/main.py
+++ bloodconnect-backend/main.py
@@ -14,7 +14,7 @@ from fastapi.middleware.cors import CORSMiddleware
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://127.0.0.1:5500"],
+    allow_origins=["http://127.0.0.1:5500", "http://127.0.0.1:5501"],
     allow_methods=["*"],
     allow_headers=["*"],
 )
